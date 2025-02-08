3cf70d8d4b5bb7eefbc272c5dc75b6e9eba3d2c3
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-08 11:26:17 +0300

    naming convention (pep8)

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-# Variables
+# Naming Convention
 
-celsius_temperature = 25
-print(celsius_temperature)
+total_count = 100 # valid
+_hidden_value = 42 # valid, but not recommended
+# 1st_place = "Gold" -> invalid, starts with digit
 
-a, b, c = 1, 2, 3
-print(a, b, c)
+# user-name = 'Alice' # invalid
+# user name = 'Bob' # invalid
 
-age, name = 25, 'Anil'
+Variable, variable, VARIABLE = 1, 2 , 3
+print(Variable, variable, VARIABLE)
+# 1 2 3
+
+# use snake_case
+max_value = 99
+total_items = 15
+
+# Const variables should be all uppercase
+PI = 3.1415
+DAYS_IN_YEAR = 365
+MAX_CONNECTIONS = 10
